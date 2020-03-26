b55a1abdca6ee97efe96115cb9f878c219cc3789
umm233 <nicemmp@gmail.com>
2020-03-26 15:07:21 +0800

    fix: add 404 page and fix a logic mistake

--- MainSever.py
+++ MainSever.py
@@ -15,12 +15,16 @@ def download(url,s,e):
     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
     down_path = "./static/img/qndxx/"+s+e+".jpg"
-    if not os.path.isfile(down_path):
-        re=r.get(url, headers=headers)
-        with open(down_path, "wb+") as f:
-            f.write(re.content)
-    else:
+    if os.path.isfile(down_path):
         pass
+    else:
+        re=r.get(url, headers=headers)
+        print(re.status_code)
+        if re.status_code == 200:
+            with open(down_path, "wb+") as f:
+                f.write(re.content)
+        else:
+            pass
 
 app = Flask(__name__)
 
@@ -32,18 +36,26 @@ def index():
 def fake_pic():
     s = str(request.form['s'])
     e = str(request.form['e'])
-    if (not (0<int(s)<20 or 0<int(e)<20)) or s=="" or e=="":
+    if (not (5<int(s)<20 and 0<int(e)<20)) or s=="" or e=="":
         return  render_template('index.html',message="错误的查询范围。")
-    elif int(s)<3:
-        return render_template('index.html',message="前两季暂时没有收录。")
+
     convert=['0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
     url=r"http://h5.cyol.com/special/daxuexi/daxuexi"+s+convert[int(e)]+e+r"/images/end.jpg"
-    print(url)
     download(url,s,e)
 
     down_img = "./static/img/qndxx/"+s+e+".jpg"
-    make_head(down_img, get_chinese(s), get_chinese(e))
-    return render_template('fake_pic.html', pic_src='img/qndxx/latest.jpg',s=get_chinese(s),e=e)
+    if os.path.isfile(down_img):
+        make_head(down_img, get_chinese(s), get_chinese(e))
+        return render_template('fake_pic.html', pic_src='img/qndxx/latest.jpg',s=get_chinese(s),e=e)
+    else:
+        return render_template('404.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000)
+    # from waitress import serve
+    # serve(app, host='0.0.0.0', port=5000)
+
+    # 本地使用
+    # app.run(host='127.0.0.1', port=5000, debug=True)
+
+    # Docker部署使用
+    app.run(host='0.0.0.0', port=5000, debug=True)
